088f18aba9c48163bf95a1cffc22039f3f7dce2e
katerynabrovko@gmail.com <katerynabrovko@gmail.com>
2020-11-03 21:54:13 +0200

    change Lesson_2_Point

--- Lesson_2/Lesson_2_Point.py
+++ Lesson_2/Lesson_2_Point.py
@@ -5,35 +5,37 @@ class Point:
         self.y = y
         self.z = z
 
-    def __repr__(self):
+    def __str__(self):
         return '{}, {}, {}'.format(self.x, self.y, self.z)
 
     def __add__(self, other):
-        return self.x + other.x, self.y + other.y, self.z + other.z
+        return Point(self.x + other.x, self.y + other.y, self.z + other.z)
 
     def __sub__(self, other):
-        return self.x - other.x, self.y - other.y, self.z + other.z
+        return Point(self.x - other.x, self.y - other.y, self.z + other.z)
+
 
     def __mul__(sefl, other):
-        return sefl.x * other.x, sefl.y * other.y, sefl.z * other.z
+        return Point(sefl.x * other.x, sefl.y * other.y, sefl.z * other.z)
 
     def __truediv__(self, other):
-        return self.x / other.x, self.y / other.y, self.z / other.z
+        return Point(self.x / other.x, self.y / other.y, self.z / other.z)
 
 
     def __pos__(self):
-        return self.x, self.y, self.z
+        return Point(self.x, self.y, self.z)
 
     def __neg__(self):
-        return -self.x, -self.y, -self.z
+        return Point(-self.x, -self.y, -self.z)
 
 
 
-a= Point(1,2,3)
+a = Point(1, 2, 3)
 b = Point(1,2,3)
-print(a + b)
+c = Point(8,4,3)
+print(a + b+ c)
 print(a * b)
-print(a - b)
+print(a + c - b)
 print(a / b)
 print(-a)
 print(-a.x)
