eb2e62959a5ffc8e1bc06f6f487a07753322bdb4
katerynabrovko@gmail.com <katerynabrovko@gmail.com>
2020-11-04 18:05:33 +0200

    Change Lesson_3_decorator

--- Lesson_3/Lesson_3_decorator.py
+++ Lesson_3/Lesson_3_decorator.py
@@ -2,29 +2,34 @@ from functools import wraps
 import requests
 import time
 
-def repeat(number):
+def repeat(number=2):
+
     def _repeat(func):
+        print(func.__name__)
 
         @wraps(func)
         def wrapper(host_):
+            total: float = 0.0
             for i in range(number):
+                t_time = time.time()
                 result = func(host_)
-                return result
+                tt_time = (time.time() - t_time)
+                print(f'time = {tt_time}')
+                total += tt_time
+            return result, total
+
         return wrapper
+
     return _repeat
 
 
 
-@repeat(8)
+@repeat(10)
 def connected(host_):
-    start_time = time.time()
     response = requests.get(host_)
-    print(f'time, {(time.time() - start_time)}')
-    print(response.status_code)
-    print(connected.__name__)
     return response.ok
 
 
-print(connected.__wrapped__('http://google.com'))
+
 result = connected('http://google.com')
 print(result)
