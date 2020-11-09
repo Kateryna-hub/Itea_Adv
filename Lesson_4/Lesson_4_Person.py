d8f44fc6cd8222a3cc570a749ede54aee5fd7de7
katerynabrovko@gmail.com <katerynabrovko@gmail.com>
2020-11-10 00:45:48 +0200

    change lesson_4_person

--- Lesson_4/Lesson_4_Person.py
+++ Lesson_4/Lesson_4_Person.py
@@ -8,7 +8,6 @@ class Person(ABC):
         self.birthday = birthday
         self.faculty = faculty
 
-
     @abstractmethod
     def show(self):
         pass
@@ -17,16 +16,17 @@ class Person(ABC):
         today = date.today()
         return today.year - self.birthday
 
+    def person_age_matching(self, from_, to_):
+        today = date.today()
+        person_age = today.year - self.birthday
+        if from_ <= person_age <= to_:
+            return self.show()
+
 
 class Applicant(Person):
 
     def show(self):
         print('{}, {}, {}'.format(self.surname, self.birthday, self.faculty))
-
-    def add_to_list(self, list_):
-        list_.append([self.surname, self.birthday, self.faculty])
-
-
 
 
 class Student(Person):
@@ -50,10 +50,17 @@ class Teacher(Person):
         print('{}, {}, {}, {}, {}'.format(self.surname, self.birthday, self.faculty, self.position, self.experience))
 
 
+applicant1 = Applicant('Ivanov', 1998, 'CS')
+applicant2 = Applicant('Popov', 1999, 'CEC')
+applicant3 = Applicant('Popov', 1998, 'ITM')
+student1 = Student('Petrov', 1993, 'CEC', 3)
+student2 = Student('Volkov', 1995, 'CS', 1)
+teacher1 = Teacher('Smirnov', 1972, 'CS', 'assistant professor ', 17)
+teacher2 = Teacher('Lebedev', 1965, 'ITM', 'professor', 35)
 
-applicant1 = Applicant('Ivanov', 1977, 'tyui')
-print(applicant1.age())
-applicant1.show()
-list = []
-applicant1.add_to_list(list)
-print(list)
+list_of_person = [applicant1, applicant2, applicant3, student1, student2, teacher1, teacher2]
+
+for item in list_of_person:
+    item.show()
+print('-' * 18)
+[item.person_age_matching(18, 25) for item in list_of_person]
