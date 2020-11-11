0ba58fe7ede45506f801a06543a7826d55431e38
katerynabrovko@gmail.com <katerynabrovko@gmail.com>
2020-11-11 18:11:41 +0200

    add lesson_4_Social

--- Lesson_4/Lesson_4_Social.py
+++ Lesson_4/Lesson_4_Social.py
@@ -1,29 +1,72 @@
-class Authorization:
+from datetime import date
 
-    def __init__(self, login, password):
-        self.login = login
-        self.password = password
+
+class Authorization:
+    list_of_users = [{'Surname': 'Petrov', 'login': 'ivan123', 'password': '1Qaz@wsx', 'data': ''},
+                     {'Surname': 'Popov', 'login': 'Petya1', 'password': '1Qaz@wsx', 'data': ''},
+                     {'Surname': 'Ivanov', 'login': '123Ivanov_I', 'password': '1Qaz@wsx', 'data': ''}
+                     ]
+
+    def __init__(self, surname, today=date.today()):
+        self.today = today
+        self.surname = surname
+        self.flag_login = False
+        self.flag_password = False
 
     def registration(self):
-        pass
+        new_login = input('Enter LOGIN: ')
+        login = self.check_login(new_login, self.list_of_users)
+        new_password = input('Enter password: ')
+        confirm_password = input('Confirm password: ')
+        if new_password == confirm_password:
+            password_ = self.check_password(new_password)
+            print(password_)
+        else:
+            print('passwords are not the same')
+            password_ = None
+            self.flag_login = '2'
+        if login[1] == '1' and password_[1] == '1':
+            self.list_of_users.append({'name': self.surname, 'login': login[0], 'password': password_[0], 'date': self.today})
+            print('Successful')
+        else:
+            print('sorry for inconvenience')
 
     def authentication(self):
         pass
 
-    def password_validation(self):
-        pass
+    @staticmethod
+    def check_password(password):
+        numbers = str('0123456789')
+        symbols_ = str('!@#$%^&*()_+-=<>,.?/{}[]')
+        while True:
+            if password != '1':
+                for letter in password:
+                    if letter not in '0123456789':
+                        print('password must contain the number')
+                    elif letter not in "!@#$%^&*()_-+=<>,.?/{}[]":
+                        print('password must contain the special symbol')
+                else:
+                    flag_password = '1'
+            else:
+                break
+            return password, flag_password
 
-    def check_login(self):
-        pass
+    @staticmethod
+    def check_login(login, list_of_users):
+        for dict_ in list_of_users:
+            if dict_.get('login') == login:
+                print('This login already exists.')
+                flag_login = '2'
+            else:
+                flag_login = '1'
+        return login, flag_login
 
 
 class User(Authorization):
 
-    def __init__(self, login, password, name, surname, date_registration, access):
-        super(User, self).__init__(login, password)
-        self.name = name
-        self.surname = surname
-        self.date_registration = date_registration
+    def __init__(self, surname, today = date.today(), access = None ):
+        super(User, self).__init__(surname, today)
+        #self.name = name
         self.access = access
 
 
@@ -40,6 +83,14 @@ class Post:
 
     def view_post(self):
         pass
+
+
+
+user = User('John')
+user.registration()
+print(Authorization.list_of_users)
+
+
 
 
 
