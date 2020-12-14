fcbb10e127d9a93b1d29c9e8f807fa7c4efc55a5
katerynabrovko@gmail.com <katerynabrovko@gmail.com>
2020-12-14 17:31:11 +0200

    add lesson 10

--- lesson_10/Blog/app_restful.py
+++ lesson_10/Blog/app_restful.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 api = Api(app)
 
 
-api.add_resource(PostResource, '/post', '/post/author/<string:author>', '/post/tag/<string:tag>')
+api.add_resource(PostResource, '/post', '/post/author/<string:author>', '/post/tag/<string:tag>', '/post/<string:id>')
 
--- lesson_10/Blog/resources.py
+++ lesson_10/Blog/resources.py
@@ -8,33 +8,42 @@ import json
 
 class PostResource(Resource):
 
-    def get(self, author=None, tag=None):
+    def get(self, author=None, tag=None, id=None):
         if author:
             posts = BlogPost.objects(author=author)
-            posts.modify(inc__post_view=1)
+            for p in posts:
+                p.modify(inc__post_view=1)
             return json.loads(posts.to_json())
         if tag:
             posts = BlogPost.objects(tags=tag)
+            for p in posts:
+                p.modify(inc__post_view=1)
+            return json.loads(posts.to_json())
+        if id:
+            posts = BlogPost.objects(id=id)
             posts.modify(inc__post_view=1)
             return json.loads(posts.to_json())
-
         else:
             posts = BlogPost.objects()
-            posts.modify(inc__post_view=1)
             return json.loads(posts.to_json())
 
     def post(self):
-
         try:
             BlogPostSchemaWrite().load(request.json)
         except ValidationError as e:
             return {'text': str(e)}
         post_ = BlogPost(**request.json).save()
-        post_.reload()
+        # post_.reload()
         return BlogPostSchemaRead().dump(post_)
 
-    def put(self):
-        pass
+    def put(self, id):
+        post = BlogPost.objects(id=id)
+        post.update(**request.json)
+        post.reload()
+        return json.loads(post.to_json())
 
-    def delete(self):
-        pass
+    def delete(self, id):
+        post = BlogPost.objects(id=id)
+        post.delete()
+        posts = BlogPost.objects()
+        return json.loads(posts.to_json())
